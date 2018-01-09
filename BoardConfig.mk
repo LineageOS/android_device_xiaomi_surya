b18440291e35d537c02b663851e3c5303d41c8d6
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    surya: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,7 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
