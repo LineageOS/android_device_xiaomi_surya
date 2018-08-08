f06874e118eff1bbac6dc85abfcb5a3285a66230
Demon Singur <demonsingur@gmail.com>
2018-08-08 19:57:04 +0000

    davinci: recovery: Include moved headers
    Change-Id: Ia0e49562dac01bf3ecd674270f2011546cfd3498

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,10 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := \
-    bootable/recovery \
-    bootable/recovery/edify/include \
-    bootable/recovery/otautil/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_xiaomi
+LOCAL_STATIC_LIBRARIES := libedify libotautil
 include $(BUILD_STATIC_LIBRARY)
