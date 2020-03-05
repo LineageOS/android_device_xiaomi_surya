514d7643883ceaa618c64437c1166424437aabee
Fenglin Wu <fenglinw@codeaurora.org>
2020-03-05 19:31:14 +0800

    davinci: Add config to exclude haptics devices from input subsystem
    Haptics devices which registers as input FF devices are not supposed to
    be detected by input subsystem. Add excluded-input-devices.xml file for
    input subsystem to exclude them.
    
    Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692

--- device.mk
+++ device.mk
@@ -406,6 +406,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
