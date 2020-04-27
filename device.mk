a807f2956747a5f14d2addc726749b73fd39db25
Yung Ti Su <andysu@google.com>
2020-04-27 14:45:07 +0800

    surya: Enabling sound trigger V2_3
    Bug: 139071862
    Test: manual
    
    Signed-off-by: Yung Ti Su <andysu@google.com>
    Change-Id: I4123b0af64fc7bed934026540136e3811fab32f3

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
