8a5d181a0e21cc6bbd9e36b5a4b01654262d6c86
Yo Chiang <yochiang@google.com>
2020-08-17 11:44:54 +0800

    surya: Use on_vendor VNDK APEX
    Move VNDK APEX to /vendor partition so we don't need to update
    vendor.img prebuilt everytime there is a change in the VNDK
    libraries.
    
    Change-Id: I832191e8876306883a04c8539cf8dcda16fe940f

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# VNDK
+PRODUCT_PACKAGES += \
+    com.android.vndk.current.on_vendor
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
