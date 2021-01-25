f020ce9da02cbaf02da14626f922dfa1317bbcf2
Arian <arian.kulmer@web.de>
2021-01-25 12:42:07 +0100

    davinci: Rename fingerprint service
    Change-Id: I09992ec66ba4b65bb60a23faa74929d385f199fa

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.davinci \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.davinci
 
 PRODUCT_COPY_FILES += \
