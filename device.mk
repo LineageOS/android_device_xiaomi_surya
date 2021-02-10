14aeb6b2528aa17bf70852192aaca7ba5fac4f16
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:58:07 +0100

    surya: Build atrace service
    Change-Id: I89b2e6c308fb4ae63680ef25b56bc20a41a2c2a4

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
