a862c5048a51e7210f4fee3c6d7427073541dc43
Edwin Wong <edwinwong@google.com>
2021-03-25 14:36:24 -0700

    surya: Uprev clearkey DRM HIDL to 1.4
    Test: atest
      atest CtsMediaTestCases:android.media.cts.MediaDrmClearkeyTest#testGetPlaybackComponent
    
    Bug: 182525516
    Change-Id: I406b7d3c2921d4698535421894c6eb3785eb1c0d

--- device.mk
+++ device.mk
@@ -155,7 +155,7 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.4-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
