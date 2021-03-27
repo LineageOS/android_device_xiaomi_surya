550927c11364b3afbbfc566f8f7ac36cb58fbd4a
Arian <arian.kulmer@web.de>
2021-03-27 17:16:52 +0100

    surya: livedisplay: Uprev to 2.1
    Change-Id: Icd9d97078cb339aaff04d7e0a52ce8b9b97d35de

--- device.mk
+++ device.mk
@@ -249,7 +249,7 @@ PRODUCT_PACKAGES += \
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.surya
+    vendor.lineage.livedisplay@2.1-service.surya
 
 # Media
 PRODUCT_PACKAGES += \
