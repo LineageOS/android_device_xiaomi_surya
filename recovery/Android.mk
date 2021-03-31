045326c52261fdb8f0ccab522e5f040cb015065e
Arian <arian.kulmer@web.de>
2021-03-31 20:57:10 +0200

    davinci: recovery: Convert to Android.bp
    Change-Id: Icc954652cd393a401caada2c7b636a4f95ea8edb

--- recovery/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := recovery_updater.cpp
-LOCAL_MODULE := librecovery_updater_xiaomi
-LOCAL_STATIC_LIBRARIES := libedify libotautil
-include $(BUILD_STATIC_LIBRARY)
