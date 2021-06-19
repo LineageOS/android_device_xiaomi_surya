7bf84ffe7a3e4458bf873a6a8917084141973075
Bruno Martins <bgcngm@gmail.com>
2021-06-19 15:25:31 +0100

    surya: Build opensource soundtrigger HAL
    Change-Id: I956d53a67841a9e1e296de6b73f08ef2efac7470

--- device.mk
+++ device.mk
@@ -66,7 +66,8 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     libvolumelistener \
     libaudio-resampler \
-    libtinycompress
+    libtinycompress \
+    sound_trigger.primary.sm6150
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
