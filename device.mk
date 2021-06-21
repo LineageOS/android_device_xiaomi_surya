1b79684beeb7efdfe5c1879c98d862b48e359c41
Willy Gardiol <willy@gardiol.org>
2021-06-21 10:49:28 +0200

    surya: set TARGET_SCREEN_HEIGHT
    Taken from device screen resolution.
    
    Change-Id: Ic8a013147f0b54cbeb0809d212af064bf03bdc4b

--- device.mk
+++ device.mk
@@ -102,7 +102,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Camera
