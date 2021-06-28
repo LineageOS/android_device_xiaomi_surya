100c8d1b211ffbf851c2ee59639a943c336b8d3b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-28 19:24:30 +0200

    surya: Be sure to build vndservicemanager
    * We need it for our blobs
    
    Change-Id: Ib757b97ee2c7d43fe606225366d729854ab987fc

--- device.mk
+++ device.mk
@@ -451,6 +451,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
