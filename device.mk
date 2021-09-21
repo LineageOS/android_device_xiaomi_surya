26447d548bac5fbbde47a87183fe8e8833258594
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-09-21 12:05:58 +0530

    surya: Drop unused SunlightEnhancement LiveDisplay HAL
    Change-Id: Ia9b8457e0619600601d2720fcc6dd1cfe4846a0a

--- device.mk
+++ device.mk
@@ -240,8 +240,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.surya
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_PACKAGES += \
