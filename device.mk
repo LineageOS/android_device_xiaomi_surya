9dec82047d087fe3acf76b7399c10d4f88c36cbf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-23 16:49:09 +0200

    surya: Move to common Xiaomi light AIDL
    Change-Id: I00494cd392d19abd97cd65aeb4ec4bd9c9b11352

--- device.mk
+++ device.mk
@@ -267,7 +267,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.surya
+    android.hardware.light-service.xiaomi
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
