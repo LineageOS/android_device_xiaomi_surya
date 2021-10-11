85ad6aa96759cc2d0b5b1a1395d81d27edd573db
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 01:00:17 +0200

    surya: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f

--- device.mk
+++ device.mk
@@ -49,7 +49,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
