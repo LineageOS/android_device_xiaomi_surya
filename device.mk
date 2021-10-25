09e355b9376a8b388c3932835140df6d6f0c75e7
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-10-25 11:13:58 +0530

    surya: Provide vendor.qti.hardware.camera.device@1.0 on vendor
     * Needed by prebuilt camera provider
    
       vndksupport: Could not load /vendor/lib64/hw/android.hardware.camera.provider@2.4-impl.so from default namespace: dlopen failed: library "vendor.qti.hardware.camera.device@1.0.so" not found: needed by /vendor/lib64/android.hardware.camera.provider@2.4-legacy.so in namespace (default).
    
    Change-Id: Ia88bd7f8fa3a94decde0c23fe4a08f47d80a7dbd

--- device.mk
+++ device.mk
@@ -109,7 +109,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libdng_sdk.vendor
+    libdng_sdk.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
     libshim_megvii
