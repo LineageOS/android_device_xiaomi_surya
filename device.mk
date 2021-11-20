d06ad90bfcc0e68e5e8ef15ff3b8eac1166e6c2d
thestrechh <thestrechh@gmail.com>
2021-11-20 01:05:22 +0000

    surya: Update sensors service
    Change-Id: I84bc6e8dce05de24128c895fceccda79b05faa9a

--- device.mk
+++ device.mk
@@ -359,7 +359,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal 
+    android.hardware.sensors@2.1-service.multihal
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
