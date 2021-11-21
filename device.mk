fa7149e3c006cf73ffb8b7f7f3a971189c9377c9
Tim Zimmermann <tim@linux4.de>
2021-11-21 20:17:51 +0100

    surya: Uprev audio HAL to V7
    Updated audio policy by running:
    
    'hardware/interfaces/audio/7.0/config/update_audio_policy_config.sh \
    device/xiaomi/sm6150-common/configs/audio/audio_policy_configuration.xml 7.0'
    
    with some manual intervention.
    
    Change-Id: Iede32c176742e89577365341732a86f6ecaee514

--- device.mk
+++ device.mk
@@ -45,8 +45,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.2-impl
