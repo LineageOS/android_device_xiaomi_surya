cdbd138dc740c58f8196d2a7be1d9ceaba325bd0
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-12-22 01:36:08 +0530

    surya: Bring back SunlightEnhancement LiveDisplay HAL
    This reverts commit 45175edb28f98076f7b78e44774bcfbfc022789b.
    
    Change-Id: I32f1e4be0463fc1ae8b3d6226d4e8341d358159f

--- device.mk
+++ device.mk
@@ -248,7 +248,8 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.surya
 
 # Media
 PRODUCT_PACKAGES += \
