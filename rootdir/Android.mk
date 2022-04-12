33423b959122b256bae00d61f43c88f864870be2
TheStrechh <thestrechh@gmail.com>
2022-04-12 19:25:05 +0530

    surya: Move init.qcom.sensors to device
    Change-Id: Ie23e8b749c680f57e0d8387e25eb8aac65cf7bb5

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -7,3 +7,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.sensors.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.qcom.sensors.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
