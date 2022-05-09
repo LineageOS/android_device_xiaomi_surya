ac5d8719c31d1ef8872b75195f95d586f429eaa0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 17:01:22 +0200

    surya: Go away Megvii
    * Nuke Megvii proprietary files and build stub ones from hardware/xiaomi
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- device.mk
+++ device.mk
@@ -110,6 +110,10 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    libMegviiFacepp-0.5.2 \
+    libmegface
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
