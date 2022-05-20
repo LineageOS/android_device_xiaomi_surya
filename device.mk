a1e7cc8caae4e30e00885dfeb5469cb9c5845415
Jiakai Zhang <jiakaiz@google.com>
2022-05-20 14:40:27 +0100

    surya: Preopt SystemUI for AOSP builds.
    Bug: 232860596
    Test: See no dex2oat invocation on the first boot.
    Change-Id: Icc38efb4b702b518caa22a3abfc7c04d6ba1ac2a

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dexpreopt
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
