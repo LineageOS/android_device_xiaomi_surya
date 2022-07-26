7e679ed38d0fad743ff2650c20f510c229294315
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2022-07-26 17:13:28 +0530

    surya: init: Fix code indentation
    Change-Id: I80168f84198e7c9250091fa584a340c5c63635a9

--- init/init_surya.cpp
+++ init/init_surya.cpp
@@ -84,19 +84,19 @@ void vendor_load_properties() {
     std::string mod_device;
 
     if (hwname == "surya") {
-	device = "surya";
+        device = "surya";
         model = "M2007J20CG";
         mod_device = "surya_global";
-	name = "surya_global";
-	fingerprint = "POCO/surya_eea/surya:11/RKQ1.200826.002/V12.5.8.0.RJGMIXM:user/release-keys";
-	description = "surya_eea-user 11 RKQ1.200826.002 V12.5.8.0.RJGMIXM release-keys";
+        name = "surya_global";
+        fingerprint = "POCO/surya_eea/surya:11/RKQ1.200826.002/V12.5.8.0.RJGMIXM:user/release-keys";
+        description = "surya_eea-user 11 RKQ1.200826.002 V12.5.8.0.RJGMIXM release-keys";
     } else if (hwname == "karna") {
-	device = "karna";
+        device = "karna";
         model = "M2007J20CI";
         mod_device = "surya_in_global";
-	name = "karna_in";
-	fingerprint = "POCO/surya_in/karna:11/RKQ1.200826.002/V12.5.8.0.RJGMIXM:user/release-keys";
-	description = "surya_in-user 11 RKQ1.200826.002 V12.5.8.0.RJGMIXM release-keys";
+        name = "karna_in";
+        fingerprint = "POCO/surya_in/karna:11/RKQ1.200826.002/V12.5.8.0.RJGMIXM:user/release-keys";
+        description = "surya_in-user 11 RKQ1.200826.002 V12.5.8.0.RJGMIXM release-keys";
    }
 
     set_ro_build_prop("fingerprint", fingerprint);
