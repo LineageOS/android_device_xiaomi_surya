8c5533675b5cb6b3fbb6fd6902e156de326d77dc
Arian <arian.kulmer@web.de>
2022-08-20 00:31:27 +0200

    surya: Improve camera shim
    Change-Id: Id11965102d7d5dce50f989c99810cda6a43aa1ec

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,9 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib64/camera/components/com.qti.node.watermark.so)
+            ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
+            ;;
     esac
 }
 
