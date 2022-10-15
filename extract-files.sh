b939d28665d5feb35341376f01770e56eda84f86
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    surya: Prevent adding same dependency twice in blob_fixup
    Change-Id: Idedeb51e027a09f64a29ac67a3a2f2a0594213f6

--- extract-files.sh
+++ extract-files.sh
@@ -56,7 +56,7 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/lib64/camera/components/com.qti.node.watermark.so)
-            ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
+            grep -q "libpiex_shim.so" "${2}" || ${PATCHELF} --add-needed "libpiex_shim.so" "${2}"
             ;;
     esac
 }
