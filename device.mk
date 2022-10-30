49f3af07ae7e333658db88178ee2d6d3f12f1998
Michael Bestas <mkbestas@lineageos.org>
2022-10-30 19:01:36 +0200

    surya: Move qti_whitelist.xml to /system_ext
    Change-Id: Id8617513cfd686b3f6888a89c1b1db196acfb476

--- device.mk
+++ device.mk
@@ -362,7 +362,7 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
 
 # RIL
