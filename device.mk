2a7e0672f8f7a42f0af6f53bbba132b17a7ef031
TheStrechh <thestrechh@gmail.com>
2022-11-22 18:10:22 +0000

    Revert "surya: use metadata_csum for /metadata"
    This reverts commit d8bd53d9401be0d7120bb8eaa7db49a7be92b228.
    
    Change-Id: I1f2efdd642547a81dee9fff47484af3fccd6dbee

--- device.mk
+++ device.mk
@@ -349,11 +349,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-PRODUCT_PACKAGES += \
-    e2fsck_ramdisk \
-    tune2fs_ramdisk \
-    resize2fs_ramdisk
-
 # Perf
 PRODUCT_PACKAGES += \
     libqti-perfd-client
