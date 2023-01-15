dd4e5323ffb3931ee1176977369732be1eab2e7f
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2023-01-15 18:15:41 +0530

    surya: Add framework compatibility matrix
    Change-Id: I169e93c51633ba05e1bb7b6bd8e6dfd27ded0da5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,10 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 4.0
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
