2bcc6e3825e872db17a805c29d71d2cd0c3ac980
TheStrechh <TheStrechh@gmail.com>
2023-01-23 19:45:29 +0000

    Revert "surya: Move init.qcom.sensors to device"
    This reverts commit 33423b959122b256bae00d61f43c88f864870be2.
    
    Change-Id: I83b26f37468fe03e06d5165abad5a90818d7ef5f

--- device.mk
+++ device.mk
@@ -400,9 +400,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
-PRODUCT_PACKAGES += \
-    init.qcom.sensors.sh
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
