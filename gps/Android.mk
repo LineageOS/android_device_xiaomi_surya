1b0dbae38f9d1cc9aa6466f2ab5568776a645aef
Michael Bestas <mkbestas@gmail.com>
2023-06-07 23:28:37 +0300

    surya: gps: Remove all Wno-error flags
    Almost all warnings/errors have been fixed.
    Ignore undefined bool conversion error on purpose.
    
    Change-Id: I00cb5f38711fa9f873da030fc6a0ebc6cde3ceeb

--- gps/Android.mk
+++ gps/Android.mk
@@ -3,20 +3,7 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 # Set required flags
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-error=unused-parameter \
-    -Wno-error=macro-redefined \
-    -Wno-error=reorder \
-    -Wno-error=missing-braces \
-    -Wno-error=self-assign \
-    -Wno-error=enum-conversion \
-    -Wno-error=logical-op-parentheses \
-    -Wno-error=null-arithmetic \
-    -Wno-error=null-conversion \
-    -Wno-error=parentheses-equality \
-    -Wno-error=undefined-bool-conversion \
-    -Wno-error=tautological-compare \
-    -Wno-error=switch \
-    -Wno-error=date-time
+    -Wno-undefined-bool-conversion
 
 LOCAL_PATH := $(call my-dir)
 include $(call all-makefiles-under,$(LOCAL_PATH))
