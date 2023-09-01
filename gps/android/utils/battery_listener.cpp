574e8ec42ed1cb0b95d7807580e7da70a323f2ca
Madhanraj Chelladurai <quic_mchellad@quicinc.com>
2023-09-01 18:22:06 +0200

    surya: gps: Add NULL check before object access
    Correct the NULL check code block and add NULL check before
    object access.
    
    Change-Id: Ic41b781b41fb4e21bbff8801d500a41a6d7219d0
    CRs-fixed: 3084543

--- gps/android/utils/battery_listener.cpp
+++ gps/android/utils/battery_listener.cpp
@@ -184,16 +184,19 @@ BatteryListenerImpl::~BatteryListenerImpl()
 {
     {
         std::lock_guard<std::mutex> _l(mLock);
-        if (mHealth != NULL)
+        if (mHealth != NULL) {
             mHealth->unregisterCallback(this);
             auto r = mHealth->unlinkToDeath(this);
             if (!r.isOk() || r == false) {
                 LOC_LOGe("Transaction error in unregister to HealthHAL death: %s",
                         r.description().c_str());
             }
+        }
     }
     mDone = true;
-    mThread->join();
+    if (NULL !=  mThread) {
+        mThread->join();
+    }
 }
 
 void BatteryListenerImpl::serviceDied(uint64_t cookie __unused,
@@ -210,7 +213,9 @@ void BatteryListenerImpl::serviceDied(uint64_t cookie __unused,
     }
     mHealth = NULL;
     mCond.notify_one();
-    mThread->join();
+    if (NULL !=  mThread) {
+        mThread->join();
+    }
     std::lock_guard<std::mutex> _l(mLock);
     init();
 }
