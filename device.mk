85a1ba59966df316419b81661635996fb7b5eabf
LuK1337 <priv.luk@gmail.com>
2023-10-19 23:08:10 +0200

    surya: Build AIDL NXP NFC HAL
    Change-Id: I6ace3ac149c84d2cb9e2773e7ef5ddbf24c35b92
    Signed-off-by: shoya0x00 <shoya0x00@gmail.com>

--- device.mk
+++ device.mk
@@ -267,7 +267,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2-service
+    android.hardware.nfc-service.nxp
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
