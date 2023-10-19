bd21db27dcfc92aa5ca861dcc0f4a9f156350e91
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    surya: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I1a9628d978cea69156d65e282f9a4ae046aa9cb2

--- device.mk
+++ device.mk
@@ -367,6 +367,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
