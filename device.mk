f0b717498bd9a2aaff62032a5b0609d94cb71680
Stefan Schmidt <thrimbor@gmail.com>
2024-02-08 23:18:10 +0100

    surya: Build Lineage Health HAL
    Change-Id: Ideba6907b3b28a45a54286653200c76ade0e7031

--- device.mk
+++ device.mk
@@ -269,6 +269,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.surya
