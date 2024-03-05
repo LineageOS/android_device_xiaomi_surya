e78663e728631dafdc20b891b3f1d572f4d6247e
The <franciscofunes2@gmail.com>
2024-03-05 17:30:53 -0300

    Add com.fingerprints.extension@1.0.vendor
    Change-Id: I152ec986a60b9fefcac397328a3e4c07ea75b5dd

--- device.mk
+++ device.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    com.fingerprints.extension@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
