667fc50f0dde3bf9b3e16515da1dc0f6e9753be6
basamaryan <basam.aryan@gmail.com>
2024-04-22 19:09:53 +0200

    surya: Use the common Xiaomi FCM
    Change-Id: I0ca395bc5c23c26e1546d3dd1839592530cc0e3f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -72,8 +72,8 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
