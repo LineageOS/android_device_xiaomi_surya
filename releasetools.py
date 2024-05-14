4f66c5b84f05a3d7ace8c65d47ee0ff4aebf6a94
Willy Gardiol <willy@gardiol.org>
2024-05-14 15:07:50 +0200

    surya: embed firmware with macro-lens fix
    This is last firmware V14.0.5.0.SJGEUXM
    
    aop.mbn: from V12.5.8.0 (fix macro lens)
    
    Change-Id: I0bfb1c2d1e71506a9ca2bf0be5383b48b89338b9

--- releasetools.py
+++ releasetools.py
@@ -30,14 +30,41 @@ def AddImage(info, basename, dest):
   path = "IMAGES/" + name
   if path not in info.input_zip.namelist():
     return
-
   data = info.input_zip.read(path)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
+def AddImageRadio(info, basename, dest):
+  name = basename
+  if ("RADIO/" + basename) in info.input_zip.namelist():
+    data = info.input_zip.read("RADIO/" + basename)
+    common.ZipWriteStr(info.output_zip, name, data)
+    info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
+    info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/bootdevice/by-name/vbmeta_system")
+
+  AddImageRadio(info, "cmnlib64.mbn", "/dev/block/bootdevice/by-name/cmnlib64")
+  AddImageRadio(info, "imagefv.elf", "/dev/block/bootdevice/by-name/imagefv")
+  AddImageRadio(info, "cmnlib.mbn", "/dev/block/bootdevice/by-name/cmnlib")
+  AddImageRadio(info, "hyp.mbn", "/dev/block/bootdevice/by-name/hyp")
+  AddImageRadio(info, "km4.mbn", "/dev/block/bootdevice/by-name/keymaster")
+  AddImageRadio(info, "tz.mbn", "/dev/block/bootdevice/by-name/tz")
+  AddImageRadio(info, "aop.mbn", "/dev/block/bootdevice/by-name/aop")
+  AddImageRadio(info, "xbl_config.elf", "/dev/block/bootdevice/by-name/xbl_config")
+  AddImageRadio(info, "BTFM.bin", "/dev/block/bootdevice/by-name/bluetooth")
+  AddImageRadio(info, "uefi_sec.mbn", "/dev/block/bootdevice/by-name/uefisecapp")
+  AddImageRadio(info, "NON-HLOS.bin", "/dev/block/bootdevice/by-name/modem")
+  AddImageRadio(info, "qupv3fw.elf", "/dev/block/bootdevice/by-name/qupfw")
+  AddImageRadio(info, "abl.elf", "/dev/block/bootdevice/by-name/abl")
+  AddImageRadio(info, "dspso.bin", "/dev/block/bootdevice/by-name/dsp")
+  AddImageRadio(info, "devcfg.mbn", "/dev/block/bootdevice/by-name/devcfg")
+  AddImageRadio(info, "storsec.mbn", "/dev/block/bootdevice/by-name/storsec")
+  AddImageRadio(info, "xbl.elf", "/dev/block/bootdevice/by-name/xbl")
+  AddImageRadio(info, "ffu.img", "/dev/block/bootdevice/by-name/ffu")
+
   return
