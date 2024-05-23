b44895db6e4ef78b406cc1782f46e6d79c85d970
Willy Gardiol <willy@gardiol.org>
2024-05-23 11:12:13 +0200

    surya: restore missing powerhint.json
    Change-Id: I943906206ed26525981ba09792d8a76f05a3574f

--- device.mk
+++ device.mk
@@ -367,6 +367,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
