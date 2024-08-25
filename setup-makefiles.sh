c0d71234f13041f3e7803ec86b252b9ae8c84d71
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 08:30:58 +0300

    surya: fix vendor.qti.hardware.fm@1.0 ELF checks
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,7 @@ function lib_to_package_fixup_vendor_variants() {
 
     case "$1" in
         com.qualcomm.qti.dpm.api@1.0 | \
+            vendor.qti.hardware.fm@1.0 | \
             vendor.qti.imsrtpservice@3.0)
             echo "$1_vendor"
             ;;
