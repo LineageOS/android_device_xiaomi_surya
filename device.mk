a0f6b4790a7d36d31bfd89a61d9cb2bbfae86ca5
basamaryan <basam.aryan@gmail.com>
2024-09-29 16:24:14 -0400

    surya: Move to Xiaomi fingerprint AIDL
    Change-Id: I68dba7184034fd37454c7d32a065f041cb611625

--- device.mk
+++ device.mk
@@ -142,7 +142,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
