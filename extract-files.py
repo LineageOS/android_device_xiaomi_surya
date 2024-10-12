c0e0cde882c06a2daaf8f8304a41512ccb0899ee
basamaryan <basam.aryan@gmail.com>
2024-10-12 14:43:53 -0400

    surya: Inherit from sm6150-common
    Change-Id: I179922aeed49eb3a60829924338a51cdfc5516f2
    Co-Authored-By: shoya0x00 <shoya0x00@users.noreply.github.com>

--- extract-files.py
+++ extract-files.py
@@ -18,26 +18,14 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/surya',
     'hardware/qcom-caf/sm8150',
-    'hardware/qcom-caf/wlan',
     'hardware/xiaomi',
-    'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
+    'vendor/xiaomi/sm6150-common',
 ]
-
-
-def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
-    return f'{lib}_{partition}' if partition == 'vendor' else None
-
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
-    (
-        'com.qualcomm.qti.dpm.api@1.0',
-        'vendor.qti.hardware.fm@1.0',
-        'vendor.qti.imsrtpservice@3.0',
-    ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
@@ -74,9 +62,10 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
-    utils = ExtractUtils.device(module)
+    utils = ExtractUtils.device_with_common(
+        module, 'sm6150-common', module.vendor
+    )
     utils.run()
