5da1d4df59d4103f9769f11aa0638e550b5d40d3
Michael Bestas <mkbestas@lineageos.org>
2025-01-13 11:03:38 +0200

    surya: Migrate mount point creation out of Android.mk
    Change-Id: Id5986b8a740e45f864eecdd2bd82d9455f128d6b

--- device.mk
+++ device.mk
@@ -314,6 +314,11 @@ PRODUCT_PACKAGES += \
     SuryaWifiResources
 
 # Partitions
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint
+
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
