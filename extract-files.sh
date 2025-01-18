74dd5cd4f0844cce8b25cf1db0d5cebc63d3d2c7
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-01-18 06:53:27 +0330

    surya: Fix soname of renamed libraries
    Change-Id: Ibdea10aa05af9e8183b72632705b6268ff3b7387

--- extract-files.sh
+++ extract-files.sh
@@ -88,6 +88,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
             ;;
+    vendor/lib64/hw/fingerprint.goodix.default.so)
+        [ "$2" = "" ] && return 0
+        "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix.default.so" "${2}"
+        ;;
         *)
             return 1
             ;;
