8c27cb52670fba9988cfded437b1ae0b7f5cde9c
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:40:58 +0800

    surya: Disable UFFD GC via `OVERRIDE_ENABLE_UFFD_GC`
    * Our kernel (4.14) does not support features needed for UFFD GC.
      Leaving it enabled results in long waiting time on the first boot.
    
    Change-Id: Ia8a17467e13d343c03ee29e514994946e7e84985

--- device.mk
+++ device.mk
@@ -392,6 +392,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# UFFD GC
+OVERRIDE_ENABLE_UFFD_GC := false
+
 # Updater
 AB_OTA_UPDATER := false
 
