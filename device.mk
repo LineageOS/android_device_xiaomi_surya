20b487bdeeec99a70e80763433d1d5ffad36c5ba
Michael Bestas <mkbestas@gmail.com>
2025-04-18 16:51:55 +0330

    surya: Enable ELF checks for libwpa_client dependent
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- device.mk
+++ device.mk
@@ -429,7 +429,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     ipacm \
     IPACM_cfg.xml \
-    libwpa_client \
     wpa_supplicant \
     wpa_supplicant.conf
 
--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -39,10 +38,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
-    (
-        'libgrallocutils',
-        'libwpa_client',
-    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
