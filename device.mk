37716baed8e1c833ae9c967f0aae03e78742347e
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-04-19 01:10:51 +0330

    Revert "surya: Speed profile services and wifi-service to reduce RAM and storage"
    This reverts commit d5195278643d1a41268bcb33e3b933dca2ed8982.
    
    Change-Id: I1f06d7491ae7cf5224d938d1c6d3820087d3d41e

--- device.mk
+++ device.mk
@@ -354,9 +354,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
-# Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
-
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
