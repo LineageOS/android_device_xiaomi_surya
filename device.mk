37cebcea66be8718a534218f6009d3483967fcd0
Mohammad Hasan Keramat J <ikeramat@libremobileos.com>
2025-05-01 21:22:28 +0330

    surya: Remove duplicate in PRODUCT_PACKAGES
    Change-Id: I0a79a17b36daab75a7d53a06d0d659bea6fcd1dc

--- device.mk
+++ device.mk
@@ -324,9 +324,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-PRODUCT_PACKAGES += \
-    init.recovery.qcom.rc
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.1-service.multihal
