95b5c20af4ff862ad5385e418cc25bda4b792b53
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    surya: switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_COPY_FILES += \
 
 # IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.example
+    android.hardware.ir-service.lineage
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
